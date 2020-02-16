29148877e9fb8285b944eb17f3512b2596a70c3a
BaeLuv <59927119+BaeLuv@users.noreply.github.com>
2020-02-17 01:38:16 +0900

    Create baekjoon2884.c

---
+++ BAE/baekjoon2884.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int h, m;
+    scanf("%d %d", &h, &m);
+
+    if (m >= 45) m -= 45;
+    else 
+    {
+        if (h == 0)
+        {
+            h = 23;
+            m += 15;
+        }
+        else
+        {
+            h--, m += 15;
+        }
+    }
+    
+    printf("%d %d", h, m);
+
+    return 0;
+}
