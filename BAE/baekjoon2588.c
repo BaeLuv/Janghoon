c78d93a714bd52ec46a8b9d0e22d762e74457001
BaeLuv <59927119+BaeLuv@users.noreply.github.com>
2020-02-17 00:44:09 +0900

    Create baekjoon2588.c

---
+++ BAE/baekjoon2588.c
@@ -0,0 +1,15 @@
+// (세 자리 수) × (세 자리 수)의 곱셈과정을 표현하는 코드
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("%d\n%d\n%d\n%d", a * (b % 10), a * ((b / 10) % 10), a * (b / 100), a * b);
+
+	return 0;
+
+}
